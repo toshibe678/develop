8b97b2aba352805fa13279105489c701c9600046
toshi <toshi@toshi.click>
2024-03-22 21:51:57 +0900

    go の テスト のテスト

--- golang/src/app/main_test.go
+++ golang/src/app/main_test.go
@@ -1,12 +1,12 @@
-package tdd_test
+package main_test
 
 import (
 	"github.com/stretchr/testify/assert"
-	main "tdd-sample"
+	"tdd-sample"
 	"testing"
 )
 
 func TestCry(t *testing.T) {
-	c1 := main.NewCat("ニャーニャー")
+	c1 := tdd.NewCat("ニャーニャー")
 	assert.Equal(t, "ニャーニャー", c1.Cry())
 }
